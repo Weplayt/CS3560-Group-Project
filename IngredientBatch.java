d2e378e86751981eb3e5ab579f33dec82f1372cc
Anson <answong929@gmail.com>
2024-10-31 15:47:06 -0700

    Added part of IngredientBatch and ProductBatch
    whats a fork? do you eat it with a steak? does making one blow up the universe?

---
+++ IngredientBatch.java
@@ -0,0 +1,14 @@
+public class IngredientBatch extends Batch 
+{
+
+    private int ingredientID; //wait are these like actual unique ints or like internal ints that represents indexes 
+    private String dateAdded; // (MM/DD/YYYY)
+    private String expirationDate; // (MM/DD/YYYY)
+    //TODO: Ingredeints in batch var
+
+    public IngredientBatch()
+    {
+
+    }
+
+}
---
+++ ProductBatch.java
@@ -0,0 +1,14 @@
+public class ProductBatch extends Batch 
+{
+
+    private int productID; //wait are these like actual unique ints or like internal ints that represents indexes 
+    private String dateAdded; // (MM/DD/YYYY)
+    private String expirationDate; // (MM/DD/YYYY)
+    //TODO: Porducts in batch var
+
+    public ProductBatch()
+    {
+
+    }
+
+}
